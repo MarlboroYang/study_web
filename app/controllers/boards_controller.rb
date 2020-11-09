dfe0091a38e626b46c1cc14c90b524dfb523b52a
MarlboroYang <yangmarlboro@gmail.com>
2020-11-09 17:22:49 +0800

    add board and new pages

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,18 @@
+class BoardsController < ApplicationController
+  def index
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    clean_params = params.require(:board).permit(:title)
+    @board = Board.new(clean_params)
+    if @board.save
+      redirect_to "/", notice: '成功新增看板'
+    else
+      render :new
+    end
+  end
+end
