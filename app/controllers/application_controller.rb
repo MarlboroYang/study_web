eb1f8a3cb497f2d1e3c63875c622b6b97e59777c
MarlboroYang <yangmarlboro@gmail.com>
2020-11-13 22:40:07 +0800

    add new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+end
+
+private
+
+def record_not_found
+  render file: 'public/404.html', 
+         layout: false, 
+         status: :not_found
 end
