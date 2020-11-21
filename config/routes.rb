5977fe9fbc6b732b85e867b6b0f4796d469bd26c
MarlboroYang <yangmarlboro@gmail.com>
2020-11-21 23:40:45 +0800

    add login sys

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root 'boards#index'
-  resources :boards 
-end
 
+  get '/users/sign_up', to: 'registrations#new', as: 'registration'
+  post '/users', to: 'registrations#create'
+
+  get '/users/sign_in', to: 'sessions#new', as: 'session'
+  post '/login', to: 'sessions#create', as: 'login'
+
+  delete '/users/sign_out', to: 'sessions#destroy', as: 'logout'
+
+  resources :boards
+end
