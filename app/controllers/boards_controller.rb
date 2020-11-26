32658259ad70321edb1d586766a2caf45ec18cfc
MarlboroYang <yangmarlboro@gmail.com>
2020-11-26 22:21:33 +0800

    change the things

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,28 +4,22 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
-
   def show
     @posts = @board.posts.order(id: :desc)
   end
-
   def new
     @board = Board.new
   end
-
   def create
     @board = Board.new(board_params)
-
     if @board.save
       redirect_to "/", notice: '成功新增看板'
     else
       render :new
     end
   end
-
   def edit
   end
-
   def update
     if @board.update(board_params)
       redirect_to root_path, notice: '更新成功'
@@ -33,17 +27,14 @@ class BoardsController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     @board.destroy
     redirect_to root_path, notice: '看板已刪除'
   end
-
   private
   def find_board
     @board = Board.find(params[:id])
   end
-
   def board_params
     params.require(:board).permit(:title)
   end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,6 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       redirect_to root_path, notice: 'OK!'
     else
